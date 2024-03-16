39982cafd418221dc3b8ca1d6c1e7cf5fb731c8c
Brian Liu <93726518+bjl0440@users.noreply.github.com>
2024-03-16 14:41:04 -0700

    Update DRV8825accel_test.ino

--- Test Programs/DRV8825accel_test/DRV8825accel_test.ino
+++ Test Programs/DRV8825accel_test/DRV8825accel_test.ino
@@ -4,7 +4,7 @@
 #define stepPin 3
 #define motorInterfaceType 1
 #define pulserate 1000
-#define accelrate 150
+#define accelrate 10000
 
 AccelStepper stepper = AccelStepper(motorInterfaceType, stepPin, dirPin);
 
@@ -15,16 +15,19 @@ void setup() {
 }
 
 void loop() {
+  Serial.print("start");
   // target position:
-  stepper.moveTo(3000);
+  stepper.moveTo(200);
+  Serial.print("moving to 3000");
   // Run to target position with set speed and acceleration/deceleration:
   stepper.runToPosition();
-
-  delay(1000);
+  Serial.print("done");
+  delay(2000);
 
   // Move back to zero:
+  Serial.print("moving back to 0");
   stepper.moveTo(0);
   stepper.runToPosition();
 
-  delay(1000);
+  delay(2000);
 }
